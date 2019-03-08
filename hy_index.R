034fbf2799db4e34f3e9d5563dfa2a1bd04aba5c
SDPOPT <houke21@gmail.com>
2019-03-09 01:43:34 +0800

    3/9 1:43

--- hy_index.R
+++ hy_index.R
@@ -22,7 +22,7 @@ data <- dbGetQuery(db, "SELECT * FROM hist_data") %>%
   filter(maturity > date) %>%
   mutate(busday = isBusinessDay("HongKong", date)) %>%
   filter(busday == "TRUE") %>%
-  mutate(monthend = endOfMonth(calendar = "HongKong", dates = date)) 
+  mutate(monthend = endOfMonth(calendar = "HongKong", dates = date))
 
 monthend <- data %>% 
   group_by(date, ID) %>%
@@ -32,109 +32,59 @@ monthend <- data %>%
 
 issuer_weight <- weight(monthend)
 
-test <- monthend %>% 
+monthbegin <- monthend %>%
+  mutate(monthbegin = monthend) %>%
+  select(monthbegin, ID, price0 = price)
+
+monthend <- monthend %>% 
   left_join(issuer_weight) %>%
   mutate(bond_weight = ifelse(fraction <= 0.5, 0,
-                              (price * amount / 100) / Cap) * weight)
+                              (price * amount / 100) / Cap) * weight) %>%
+  select(date, ID, weight = bond_weight)
+
+
+return_index <- data %>%
+  left_join(monthend) %>%
+  group_by(ID) %>%
+  arrange(-desc(date)) %>%
+  mutate(weight = lag(weight),
+         count = ifelse(is.na(weight) == TRUE, 0, 1),
+         count = cumsum(count)) %>%
+  filter(count > 0) %>%
+  mutate(weight = na.locf(weight)) %>%
+  ungroup() %>% select(-count) %>%
+  mutate(monthbegin = endOfMonth(calendar = "HongKong", dates = monthend - 32)) %>%
+  left_join(monthbegin) %>%
+  ungroup() %>%
+  group_by(ID) %>%
+  mutate(date0 = rep(monthbegin[1], times = NROW(date))) %>%
+  group_by(ID, date) %>%
+  mutate(fraction = yearFraction(date0, date, 6),
+         interest = coupon * fraction,
+         value0 = ifelse(date == monthend, price + interest, NA)) %>%
+  ungroup() %>%
+  group_by(ID) %>%
+  arrange(-desc(date)) %>%
+  mutate(value0 = c(price0[1], value0[1 : (NROW(value0) - 1)]),
+         value0 = na.locf(value0),
+         return = (price + interest) / value0) %>%
+  ungroup() %>%
+  group_by(monthend, date) %>%
+  summarise(return = sum(weight * return)) %>%
+  ungroup()
   
 
+monthend <- return_index %>%
+  filter(date == monthend) %>%
+  mutate(return0 = cumprod(return)) %>%
+  select(date, return0)
+  
+return_index <- return_index %>%
+  left_join(monthend) %>%
+  mutate(return0 = c(1, return0[1 : (NROW(return0) - 1)]),
+         return0 = na.locf(return0)) %>%
+  mutate(index = return * return0)
 
-index_return <- function(db, date1, date2) {
-
-date1 <- as.Date(date1)
-date2 <- as.Date(date2)
-
-data <- dbGetQuery(db, sprintf("SELECT date, ID, PX_MID 
-                                   FROM hist_data 
-                                   WHERE date(date) 
-                                   BETWEEN date('%s') 
-                                   AND date('%s')", 
-                                  date1 - 1, date2)) %>%
-  mutate(date = as.Date(date)) %>%
-  filter(is.na(PX_MID) == FALSE)
-
-data1 <- data %>% 
-  filter(date >= date1) %>% 
-  mutate(price1 = PX_MID) %>%
-  select(-PX_MID)
-
-data0 <- data %>%
-  filter(date < date2) %>%
-  mutate(date = date + 1,
-         price0 = PX_MID) %>%
-  select(-PX_MID)
-
-data <- data0 %>% 
-  left_join(data1) %>%
-  filter(is.na(price1) == FALSE)
-
-bond <- bond_pool(db) %>% select(ID, Coupon, Issued)
-
-data <- data %>% left_join(bond) %>% 
-  group_by(ID) %>%
-  mutate(date0 = date[1],
-         date1 = date[length(date)]) %>%
-  ungroup()
-
-trade <- as.tibble(c(sort(unique(data$date0)),
-                     sort(unique(data$date1))))
-names(trade) <- "date"
-trade <- trade %>% 
-  filter(duplicated(date) == FALSE) %>%
-  filter(date != date2) %>%
-  arrange(-desc(date)) %>% 
-  mutate(trade = 1, tradeID = c(1 : length(trade)))
-
-data <- data %>% 
-  left_join(trade) %>% 
-  arrange(-desc(date)) %>%
-  group_by(ID) %>%
-  mutate(value = na.locf(Issued * price0 * trade),
-         tradeID = na.locf(tradeID)) %>%
-  select(-date0, -date1, -trade) %>% ungroup()
-
-return <- data %>%
-  mutate(interest = Coupon / 365) %>%
-  group_by(ID, tradeID) %>%
-  arrange(-desc(date)) %>%
-  mutate(price_return = price1 / price0[1], 
-         interest_return = interest / price0[1]) %>% ungroup() %>%
-  group_by(date) %>%
-  mutate(weight = value / sum(value)) %>%
-  summarise(price_return = sum(price_return * weight),
-            interest_return = sum(interest_return * weight)) %>%
-  ungroup() %>% left_join(trade) %>%
-  mutate(tradeID = na.locf(tradeID)) %>%
-  group_by(tradeID) %>%
-  mutate(interest_return = cumsum(interest_return),
-         total_return = price_return + interest_return) %>%
-  ungroup() %>%
-  mutate(trade = c(trade[2 : length(trade)], NA),
-         price = cumprod(ifelse(is.na(trade) == TRUE, 1, price_return)),
-         total = cumprod(ifelse(is.na(trade) == TRUE, 1, total_return))) %>%
-  mutate(price = na.locf(c(1, price[1 : length(price) - 1])),
-         total = na.locf(c(1, total[1 : length(total) - 1]))) %>%
-  mutate(price_return = price_return * price,
-         total_return = total_return * total) %>%
-  select(date, price_return, total_return)
-
-  return(return)
-}
-
-
-index_return_plot <- function(return) {
-price_return <- xts(x = return$price_return * 100, 
-                    order.by = return$date)
-total_return <- xts(x = return$total_return * 100,
-                    order.by = return$date)
-
-result <- cbind(price_return, total_return)
-names(result) <- c("price return", "total return")
-
-dygraph(result, main = "China US HY Index", 
-        group = "result") %>% dyRangeSelector()
-
-}
 
 weight <- function(data) {
   
